b646c9557b497570166e7c2e03088681d9b91235
Erland <erland.handegard@outlook.com>
2022-12-05 18:48:55 +0100

    Erland roter det til

--- 2oppgave1.py
+++ 2oppgave1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 yverdier = [5, 3, 6, 3, 5, 1, 2, 2, 2, 4, 2, 2, 5, 5, 6, 3, 5, 3, 5, 4, 2, 6,
             1, 4, 2, 3, 3, 3, 5, 5]
 
-
+erland kødder det til
 
 plt.bar(yverdier)
 plt.show()
